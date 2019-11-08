5c76430eab21fbe81514a459e51d542297d36c60
Emily Turner (RIT Student) <ekt6170@ad.rit.edu>
2019-11-08 15:33:30 -0500

    Fixing render data

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -8,10 +8,7 @@ const makerPage = (req, res) => {
       return res.status(400).json({ error: 'An error occurred' });
     }
 
-    return res.render('app', { csrfToken: req.csrfToken(), domos: docs },
-    {
-      style: req.body.theme,
-    });
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs, style: req.body.theme });
   });
 };
 
